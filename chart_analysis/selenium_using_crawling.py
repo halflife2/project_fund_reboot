bb160e5a5a1bbf6fa7cb70eb71b848c43000d14e
Jeon se won <rainmeter92@gmail.com>
2018-12-05 06:30:36 +0900

    multiple href crawler

--- chart_analysis/selenium_using_crawling.py
+++ chart_analysis/selenium_using_crawling.py
@@ -18,15 +18,8 @@ driver = webdriver.Firefox(executable_path='/Users/ItsFriday/Documents/GitHub/pr
 driver.implicitly_wait(30)
 driver.get('https://kr.investing.com/stock-screener/?sp=country::11|sector::a|industry::a|equityType::a%3Ceq_market_cap;1')
 
-xpath = "//table[@id='resultsTable']/tbody/tr"
-box = driver.find_element_by_xpath(xpath)
-box2 = box.find_elements_by_css_selector('a').get_attribute('href')
-for item in box2:
-    data = item
-    print(data, end='')
-# for i in box:
-#     print(i, '\n')
-
-#listed_box = [item.text for item in enumerate(box) if item != 0]
-# for item in box:
-#     print(item.text)
+xpath = "//table[@id='resultsTable']/tbody"
+element = driver.find_element_by_xpath(xpath)
+elements = [item.get_attribute('href') for item in element.find_elements_by_css_selector('a')]
+for item in elements:
+    print(item)
