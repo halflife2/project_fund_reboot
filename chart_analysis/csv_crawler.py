fb4237df35fdd9f506f7fcd829773ea587331f10
Jeon se won <rainmeter92@gmail.com>
2018-12-10 19:05:16 +0900

    add url_crawler

--- chart_analysis/csv_crawler.py
+++ chart_analysis/csv_crawler.py
@@ -16,12 +16,27 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 class SeleniumCrawler:
     def __init__(self):
         if platform.system() == 'Darwin':
-            exe_path = '/Users/ItsFriday/Documents/GitHub/project_fund/geckodriver(mac)'
+            exe_path = '/Users/ItsFriday/Documents/GitHub/project_fund/geckodriver'
         elif platform.system() == 'Windows':
             exe_path = '/project1/project_fund_reboot/geckodriver(win64)'
 
         self.driver = webdriver.Firefox(executable_path=exe_path)
         self.driver.implicitly_wait(3)
+
+    def url_crawler(self):
+        for i in range(1, 46):
+            url_part_1 = 'https://kr.investing.com/stock-screener/?sp=country::11|'
+            url_part_2 = 'sector::a|industry::a|equityType::a%3Ceq_market_cap;' + str(i)
+            total_url = url_part_1 + url_part_2
+            self.driver.get(total_url)
+
+            xpath = "//table[@id='resultsTable']/tbody"
+            element = self.driver.find_element_by_xpath(xpath)
+            elements = [item.get_attribute('href') for item in element.find_elements_by_css_selector('a')]
+
+            with open('total_url.txt', 'at') as f:
+                for item in elements:
+                    f.write(item + '\n')
 
     def auto_login(self, email, password):
 
@@ -40,10 +55,9 @@ class SeleniumCrawler:
 
     def download_csv(self, input_start_date, input_end_date):
 
-        with open('seperated_5.txt', 'rt') as f:
+        with open('seperated_1.txt', 'rt') as f:
 
             for key, URL in enumerate(f.readlines()):
-                print(URL)
 
                 self.driver.get(URL)
                 self.driver.implicitly_wait(3)
@@ -64,11 +78,13 @@ class SeleniumCrawler:
 
                 download = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='float_lang_base_2 downloadDataWrap']/a[@title='데이터 다운로드']")))
                 download.click()
-                print('('+ str(key)+ '/' + '100' + ')' + 'done. ', end=' ')
+
+                stream = '( '+ str(key)+ ' / 100)' + 'done. '
+                print(stream)
 
 
 
 
 Stocks = SeleniumCrawler()
-Stocks.auto_login(email='your_id', password='your_password')
+Stocks.auto_login(email='your_email', password='Your_password')
 Stocks.download_csv(input_start_date='2012/01/01', input_end_date='2018/11/30')
